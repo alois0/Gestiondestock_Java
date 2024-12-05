64a8022bb24c00e4ccb36dca2fa1cd9d4b88e0ed
Nicolab78 <nicobou60@gmail.com>
2024-12-05 11:43:22 +0100

    modif bdd, changement possible

--- gestion_stock_java/src/app/App.java
+++ gestion_stock_java/src/app/App.java
@@ -5,8 +5,6 @@ import vue.ProduitView;
 
 public class App {
     public static void main(String[] args) {
-        ProduitController produitController = new ProduitController();
-        ProduitView produitView = new ProduitView(produitController);
-        produitView.setVisible(true);
+       
     }
 }
